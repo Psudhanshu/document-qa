06c8acef97b4111281f8c45f57fa7bc043bf60dc
Sudhanshu Pawar <62494317+Psudhanshu@users.noreply.github.com>
2024-09-12 14:06:40 +0000

    lab commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 page1 = st.Page("lab1.py", title="Lab1")
-page2 = st.Page("lab2.py", title="Lab2")
-
-pg = st.navigation([page1,page2])
+# page2 = st.Page("HW2.py", title="Lab2")
+page2 = st.Page("HW2.py",title="Lab2")
+page3 = st.Page("Lab3.py",title="Lab3")
+pg = st.navigation([page1,page2,page3])
 st.set_page_config(page_title="all Labs")
 pg.run()
 
